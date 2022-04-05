80f15f7d2100af4fc37e68764812120ffd8d0370
Amit Pandit <90578817+amit0693@users.noreply.github.com>
2022-04-05 12:55:18 -0400

    init1

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,7 +7,7 @@ module.export = {
   networks: {
     
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/D4otqRh61botVWA3WG5KsYVMilRai8ev',
+      url: 'https://eth-ropsten.alchemyapi.io/v2/TJPaDaRhcrbW6EBa6Ff3-qsj64-GIns2',
       accounts: ['42686849cee34073eab476fe4a3937de2e5749ebba7bfc85889e40daeb9c6adb'],
     },
   },
